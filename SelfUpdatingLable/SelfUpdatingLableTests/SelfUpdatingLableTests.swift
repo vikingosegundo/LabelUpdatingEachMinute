660dff64cbdd5b45da1bd8beb888294e8fff405c
Manuel Meyer <manuel.meyer@goeuro.com>
2015-02-25 08:28:36 +0100

    fixed copyright notes

--- SelfUpdatingLable/SelfUpdatingLableTests/SelfUpdatingLableTests.swift
+++ SelfUpdatingLable/SelfUpdatingLableTests/SelfUpdatingLableTests.swift
@@ -3,7 +3,7 @@
 //  SelfUpdatingLableTests
 //
 //  Created by Manuel Meyer on 24/02/15.
-//  Copyright (c) 2015 GoEuro. All rights reserved.
+//  Copyright (c) 2015 Manuel Meyer All rights reserved.
 //
 
 import UIKit
